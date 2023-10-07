4a1d122c40a7a8878ec36cd578e8fd8422432f62
Hoa Truong <huynhhoapy97@gmail.com>
2023-10-07 21:33:04 +0700

    Learn Composite Pattern

--- Composite/src/com/huynhhoapy97/Main.java
+++ Composite/src/com/huynhhoapy97/Main.java
@@ -19,5 +19,6 @@ public class Main {
         List<Product> productList = Arrays.asList(product_1, product_2, product_3);
         PackageComposite packageComposite = new PackageComposite(productList);
         System.out.println("Total price: " + packageComposite.calculatePrice());
+
     }
 }
